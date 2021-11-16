a4890043d849aa8aefa6a95bb425c3597303986f
Michael Krause <Michael.Krause@e-Mail.de>
2021-11-16 14:25:52 +0100

    Update Main
    Main

---
+++ src/main/java/de/mkccl/flyingbarber/FlyingbarberApplication.java
@@ -0,0 +1,13 @@
+package de.mkccl.flyingbarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingbarberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyingbarberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/mkccl/flyingbarber/FlyingbarberApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mkccl.flyingbarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyingbarberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
