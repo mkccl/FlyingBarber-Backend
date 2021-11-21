4c3b813640904bc1060f63fe3cf9e0c467bd0b1f
Michael Krause <Michael.Krause@e-Mail.de>
2021-11-21 20:05:08 +0100

    update
    update

--- src/main/java/de/mkccl/flyingbarber/calender/controller/CalenderController.java
+++ src/main/java/de/mkccl/flyingbarber/calender/controller/CalenderController.java
@@ -4,15 +4,13 @@ import de.mkccl.flyingbarber.calender.model.EmployeeModel;
 import de.mkccl.flyingbarber.calender.model.MonthModel;
 import de.mkccl.flyingbarber.calender.service.impl.CalenderServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
 @RestController
 @RequestMapping("api/v1/calender")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CalenderController {
 
     @Autowired
